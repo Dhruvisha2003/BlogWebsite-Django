541344b8b3d7aed1822082ea16faac4a4e440db7
dhruvkumar <druvzanzmera@gmail.com>
2024-10-11 09:05:46 +0530

    updated

--- app1/models.py
+++ app1/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class slide(models.Model):
-    image = models.ImageField()
-    caption = models.CharField(max_length=250)
 
 class category(models.Model):
     img = models.ImageField()
     name = models.CharField(max_length=50)
+
+    def __str__(self): 
+         return self.name
 
 class blog(models.Model):
     image = models.ImageField()
@@ -17,9 +17,20 @@ class blog(models.Model):
     title = models.CharField(max_length=250)
     desc = models.CharField(max_length=10000)
 
+    def __str__(self):
+        return self.title
+
+
+class slide(models.Model):
+    image = models.ImageField()
+    name = models.ForeignKey(category,on_delete=models.CASCADE,default=1)
+
 class Myprofile(models.Model):
     img = models.ImageField()
     name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=250)
+    desc = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.name
 
 
